33a81bc2dcaba5c3a6ac5c423e1d37dc5b838124
Naveen Pinumalla <Naveen.mca2019@gmail.com>
2025-02-04 09:51:09 +0530

    Adding text from E: drive to package location

--- IOPackage/WriteToFile.java
+++ IOPackage/WriteToFile.java
@@ -8,15 +8,16 @@ public class WriteToFile {
     public static void main(String[] args) {
         try {
             BufferedWriter br = new BufferedWriter(new FileWriter("./Files/TextFile.txt"));
-            br.write("Writing to a text file");
-            br.close();
+            //br.write("Writing to a text file");
+            
 
             BufferedReader reader = new BufferedReader(new FileReader("E:/Visual_Studio/Questions.txt"));
             String line;
             while((line = reader.readLine()) != null){
-                System.out.println(line);
+                //System.out.println(line);
+                br.write(line+"\n");
             }
-            reader.close();
+            reader.close();br.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
